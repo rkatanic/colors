6d224c618ccbbaf4bfe8447bbca0fbf8ccee6bcd
rkatanic <rkatanic@outlook.com>
2022-10-22 22:20:58 +0200

    Updates color console

--- components/ColorsExport.tsx
+++ components/ColorsExport.tsx
@@ -25,7 +25,7 @@ const ColorsExport = ({ colors }: Props): JSX.Element => {
           <div
             key={tab}
             onClick={() => setActiveTab(tab)}
-            className={`cursor-pointer p-2 px-4 -mb-[1px] border z-10 ${
+            className={`cursor-pointer p-2 px-4 -mb-[0px] border  ${
               activeTab === tab
                 ? "rounded-t-lg bg-neutral-900 border-neutral-800 border-b-transparent text-white"
                 : "border-transparent text-neutral-500"
@@ -35,48 +35,58 @@ const ColorsExport = ({ colors }: Props): JSX.Element => {
           </div>
         ))}
       </div>
-      <div className="border border-neutral-800 p-4 rounded-md rounded-tl-none relative">
+      <div className="border border-neutral-800 p-4 rounded-md rounded-tl-none -mt-[1px]">
         {activeTab === TABS[0] &&
           colorShades.map((colorShade, i) => (
             <div key={i} className="font-mono text-sm text-neutral-300 ">
-              hsl({colorShade.h}, {colorShade.s}%, {colorShade.l}%)
+              <span className="text-neutral-500">{Object.keys(colors)[i]}</span>{" "}
+              : "hsl({colorShade.h}, {colorShade.s}%, {colorShade.l}%)"
               {i !== 9 && ","}
             </div>
           ))}
         {activeTab === TABS[1] &&
           colorShades.map((colorShade, i) => (
             <div key={i} className="font-mono text-sm text-neutral-300 ">
-              {HSLToHex(colorShade.h, colorShade.s, colorShade.l)}
+              <span className="text-neutral-500">{Object.keys(colors)[i]}</span>{" "}
+              : {HSLToHex(colorShade.h, colorShade.s, colorShade.l)}"
               {i !== 9 && ","}
             </div>
           ))}
         {activeTab === TABS[2] &&
           colorShades.map((colorShade, i) => (
             <div key={i} className="font-mono text-sm text-neutral-300 ">
-              --color-{Object.keys(colors)[i]}: hsl({colorShade.h},{" "}
-              {colorShade.s}%, {colorShade.l}%);
+              <span className="text-neutral-500">
+                --color-{Object.keys(colors)[i]}
+              </span>
+              : hsl({colorShade.h}, {colorShade.s}%, {colorShade.l}%);
             </div>
           ))}
         {activeTab === TABS[3] &&
           colorShades.map((colorShade, i) => (
             <div key={i} className="font-mono text-sm text-neutral-300 ">
-              --color-{Object.keys(colors)[i]}:{" "}
-              {HSLToHex(colorShade.h, colorShade.s, colorShade.l)};
+              <span className="text-neutral-500">
+                --color-{Object.keys(colors)[i]}
+              </span>
+              : {HSLToHex(colorShade.h, colorShade.s, colorShade.l)};
             </div>
           ))}
         {activeTab === TABS[4] &&
           colorShades.map((colorShade, i) => (
             <div key={i} className="font-mono text-sm text-neutral-300 ">
-              $color-{Object.keys(colors)[i]}: hsl({colorShade.h},{" "}
-              {colorShade.s}%, {colorShade.l}%);
+              <span className="text-neutral-500">
+                $color-{Object.keys(colors)[i]}
+              </span>
+              : hsl({colorShade.h}, {colorShade.s}%, {colorShade.l}%);
             </div>
           ))}
 
         {activeTab === TABS[5] &&
           colorShades.map((colorShade, i) => (
             <div key={i} className="font-mono text-sm text-neutral-300 ">
-              $color-{Object.keys(colors)[i]}:{" "}
-              {HSLToHex(colorShade.h, colorShade.s, colorShade.l)};
+              <span className="text-neutral-500">
+                $color-{Object.keys(colors)[i]}
+              </span>
+              : {HSLToHex(colorShade.h, colorShade.s, colorShade.l)};
             </div>
           ))}
       </div>
